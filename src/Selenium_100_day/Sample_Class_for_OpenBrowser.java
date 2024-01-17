5c9458b2da6d6f3b2a9554b7ce067d9895c7a510
SanthoshKrishnasamy <santhoshronaldoj@gmail.com>
2024-01-17 09:46:50 +0530

    First commit

---
+++ src/Selenium_100_day/Sample_Class_for_OpenBrowser.java
@@ -0,0 +1,18 @@
+package Selenium_100_day;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Sample_Class_for_OpenBrowser {
+
+	public static void main(String[] args) {
+		
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\santh\\eclipse-workspace\\selenium\\resource\\geckodriver.exe");
+			
+			WebDriver driver = new FirefoxDriver();
+			
+			driver.manage().window().maximize();
+			
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+			
+	}}
